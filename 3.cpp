df36df02ec25999cb0f0420adbfb85134cc069c4
notSherry <1204800782@qq.com>
2017-02-22 10:54:05 +0800

    2017/2/22 update3

--- 3.cpp
+++ 3.cpp
@@ -4,35 +4,46 @@
 
 #include "LeetCode.h"
 
-//求最长子串
+//求最长不重复子串，利用两重循环，判断重复与否，要注意边界的判断。
+
 int lengthOfLongestSubstring(string s) {
-    vector<int> table(200, 0);
-    int count = 0;
     int max_len = 0;
-    if (s.length() == 0) {
-        max_len = 0;
+    if (s.length() == 1) {
+        return 1;
     }
-    int k = 0;
+
     for (int i = 0; i < s.length(); i++) {
-        if (table[s[i]] < k+1) {
-            count++;
-            table[s[i]] = k+1;
-        } else {
-            count = 1;
-            k++;
+        int table[300];
+        for (int k = 0; k < 300; k++) {
+            table[k] = 0;
         }
-        if (count > max_len) {
-            max_len = count;
+        table[s[i]] = 1;
+        for (int j = i + 1; j < s.length(); j++) {
+            if (table[s[j]] == 1) {
+                if (j - i > max_len) {
+                    max_len = j - i;
+                }
+                break;
+            }
+            //当向后的子串一直没有重复时，直至最后一个字符，截止条件。
+            if (j == s.length() - 1) {
+                if (j - i + 1 > max_len) {
+                    max_len = j - i + 1;
+                }
+                break;
+            }
+            table[s[j]] = 1;
         }
     }
     return max_len;
+
 }
 
-int main(){
+int main() {
     string s = "dvdf";
 
     int result = lengthOfLongestSubstring(s);
 
-    cout << result<<endl;
+    cout << result << endl;
     return 0;
 }
