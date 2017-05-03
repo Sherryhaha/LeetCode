41f87bc8409f269b960ce1b32694243140e9dd4f
notSherry <1204800782@qq.com>
2017-05-03 09:13:31 +0800

    2017／5／3 update

---
+++ 28.cpp
@@ -0,0 +1,48 @@
+//
+// Created by sunguoyan on 2017/5/3.
+//实现判断needle是否在haystack中，如果在，返回第一次出现的下标
+
+//
+#include "LeetCode.h"
+
+
+int strStr(string haystack, string needle) {
+    int len_hay = haystack.size();
+    int len_needle = needle.size();
+    if(len_hay < len_needle){
+        return -1;
+    }
+    if(len_needle==0){
+        return 0;
+    }
+   // bool flag = false;
+  //  int result;
+    for(int i = 0;i<len_hay-len_needle+1;i++){
+        if(haystack[i] == needle[0]) {
+            int j;
+            for (j = 1; j < len_needle;) {
+                char test = haystack[i+j];
+                char test2 = needle[j];
+                cout<<test<<"----"<<test2<<endl;
+                if(haystack[i+j]==needle[j]){
+                    j++;
+                }else{
+                    break;
+                }
+            }
+            if(j==len_needle){
+               // flag = true;
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+
+int main(){
+    string haystack = "a";
+    string needle = "a";
+    int result = strStr(haystack,needle);
+    cout<<result<<endl;
+    return 0;
+}
